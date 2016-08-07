00677824c14b398e5011f1205a1e83d76b01dd3a
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-08-06 22:32:44 -0400

    Add project files.

---
+++ FinalAssignment-Part1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalAssignment_Part1.MvcApplication" Language="C#" %>
---
+++ FinalAssignment-Part1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalAssignment_Part1.Startup))]
+namespace FinalAssignment_Part1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
